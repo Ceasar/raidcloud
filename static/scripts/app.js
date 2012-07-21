853dbf51b76720b2d1b09c3ded478c375325520f
Adi Dahiya <adi.dahiya14@gmail.com>
2012-07-21 15:19:50 -0400

    add backbone and underscore

---
+++ static/scripts/app.js
@@ -0,0 +1,15 @@
+/* Javascript entry point for our app */
+define(function (require) {
+  'use strict';
+
+  var $           = require('lib/jquery')
+    , _           = require('lib/underscore.min')
+    , Backbone    = require('lib/backbone.min');
+
+  require('lib/bootstrap-modal');
+
+  console.log('init');
+
+
+
+});
