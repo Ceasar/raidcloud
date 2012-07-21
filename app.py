d56d8192ff4b278015d67091dd32428c5a6dad85
Ceasar Bautista <cbautista2010@gmail.com>
2012-07-21 14:40:37 -0400

    Mock routes.

--- app.py
+++ app.py
@@ -3,11 +3,33 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
+def index():
     return 'Hello World!'
+
+
+@app.route('/users')
+def show_user(id):
+    return {}
+
+
+@app.route('/users/<id>')
+def show_user(id):
+    return {}
+
+
+@app.route('/files')
+def show_files(id):
+    return {}
+
+
+@app.route('/files/<id>')
+def show_file(id):
+    return {}
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
