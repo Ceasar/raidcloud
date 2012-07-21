3f8ea0d4a5c6ec2fe177c8e0c9104abafb8aedb2
Adi Dahiya <adi.dahiya14@gmail.com>
2012-07-21 15:12:05 -0400

    set up requirejs

---
+++ static/scripts/main.js
@@ -0,0 +1,14 @@
+/* Javascript entry point for our app */
+define(function (require) {
+  'use strict';
+
+  var $           = require('jquery')
+    , plugin      = require('jquery.plugin');
+
+  require('bootstrap/bootstrap-modal');
+
+  console.log('init');
+
+
+
+});
