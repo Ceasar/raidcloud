ccecf5e295056f2815f0f157795d71adcbded1fc
Nick Meyer <nicholasmeyer@gmail.com>
2012-07-21 17:07:51 -0400

    Move models back to app

--- app.py
+++ app.py
@@ -11,7 +11,37 @@ db = SQLAlchemy(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://raid:cloud@localhost/raidcloud'
 
-from models import User
+###
+# Models
+###
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+
+    drive_id = db.Column(db.BigInteger, nullable=True)
+    drive_token = db.Column(db.String(255), nullable=True)
+
+    dropbox_id = db.Column(db.BigInteger, nullable=True)
+    dropbox_token = db.Column(db.String(255), nullable=True)
+
+    files = db.relation('File', backref='user')
+
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    chunks = db.relation('Chunk', backref='file')
+
+
+class Chunk(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    file_id = db.Column(db.Integer, db.ForeignKey('file.id'), nullable=False)
+
+###
+# Routes
+###
 
 @app.before_request
 def before_request():
