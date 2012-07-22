1e7bf3696a6cfe68a1b1b46470811993e546a75d
Adi Dahiya <adi.dahiya14@gmail.com>
2012-07-22 09:09:09 -0400

    add support for storage quotas on front end

--- static/scripts/models/user.js
+++ static/scripts/models/user.js
@@ -50,10 +50,24 @@ define(function (require, exports) {
     }
 
   , initProviders: function () {
-      if (this.get('has_dropbox')) this.get('providers').dropbox.active = true;
-      if (this.get('has_google')) this.get('providers').drive.active = true;
-      if (this.get('has_box')) this.get('providers').box.active = true;
-      if (this.get('has_sugarsync')) this.get('providers').box.active = true;
+      var providers = this.get('providers');
+
+      if (this.get('has_dropbox')) {
+        providers.dropbox.active = true;
+        providers.dropbox.used = Math.round(this.get('dropbox_total') / this.get('dropbox_quota'));
+      }
+      if (this.get('has_google')) {
+        providers.drive.active = true;
+        providers.drive.used = Math.round(this.get('drive_total') / this.get('drive_quota'));
+      }
+      if (this.get('has_box')) {
+        providers.box.active = true;
+        providers.box.used = Math.round(this.get('box_total') / this.get('box_quota'));
+      }
+      if (this.get('has_sugarsync')) {
+        providers.sugarsync.active = true;
+        providers.sugarsync.used = Math.round(this.get('sugarsync_total') / this.get('sugarsync_quota'));
+      }
     }
 
   });
